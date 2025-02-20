6b930ee30e6393124884bb554b890d0c786daaca
asansan2005 <alejandro.sanchez-sanchez@iesruizgijon.com>
2025-02-20 16:43:28 +0100

    Suma y Resta Calculadora

--- src/calculadora/Funciones_CALCULADORA.java
+++ src/calculadora/Funciones_CALCULADORA.java
@@ -47,14 +47,4 @@ public class Funciones_CALCULADORA {
         double resta=numero1-numero2;
         return resta;
     }
-
-    public double multiplicacion(){
-        double multiplicacion=numero1*numero2;
-        return multiplicacion;
-    }
-    public double division() {
-        double division=numero1/numero2;
-        return division;
-    }
-
 }
